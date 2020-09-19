e9aa1b6d9755dd142f9a6fe7b913a0db9c745ee0
Aleksander Apostolidis <alex.ca.dev@gmail.com>
2020-09-19 12:33:22 +0200

    Add lesson67

---
+++ Lesson67/vue-axios/src/views/EventList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import api from '@/api/AppApi'
+
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    api
+      .getEvents()
+      .then(request => {
+        this.events = request.data
+      })
+      .catch(error => {
+        console.log('Some error occurred' + error)
+      })
+  }
+}
+</script>
